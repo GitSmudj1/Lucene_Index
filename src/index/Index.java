157b699039f66e7b3b8ae73b91f8886b7090eac8
tasinclair1 <tasinclair1@gmail.com>
2015-11-20 14:44:45 +0000

    Started piecing the application together

---
+++ src/index/Index.java
@@ -0,0 +1,23 @@
+package index;
+
+import model.DataModel;
+
+/**
+ * Created by thomassinclair on 16/11/15.
+ */
+public class Index {
+
+    private DataModel dataModel;
+
+    public Index(DataModel dataModel) {
+
+        this.dataModel = dataModel;
+
+    }
+
+    public void generateIndexes() {
+
+
+
+    }
+}
