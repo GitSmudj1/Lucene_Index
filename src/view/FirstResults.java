6dfd9fcae8fcea38f17f0155150fc1c8844ddaa5
Aidan Smith <Aidan Smith>
2015-12-18 15:55:46 +0000

    Fixed bugs

--- src/view/FirstResults.java
+++ src/view/FirstResults.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
@@ -26,47 +27,50 @@ public class FirstResults extends JFrame {
 	private ActionListener listener;
 	public MouseListener catListener;
 	public MouseListener resListener;
-	
+
 	private Results results;
 	private List<String> categoryList;
-	
+
 	private JList res;
-	
+
 	private JScrollPane resPane;
-	
+
 	private DataSet dataSet;
-	
+
 	private List<String> fileNames;
-	
+
 	private String searchTerm;
 
 	/**
 	 * Create the frame.
 	 */
 	public FirstResults(Results results, DataSet dataSet, String searchTerm) {
-		
+
 		this.searchTerm = searchTerm;
 		fileNames = new ArrayList();
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		listener = new RunListener(this, dataSet);
 		
+		
+		
+
+		listener = new RunListener(this, dataSet);
+
 		this.results = results;
 		categoryList = results.getKeys();
-		
+
 		JList<String> cat = new JList(categoryList.toArray());
 
 		JScrollPane catPane = new JScrollPane(cat);
-		
+
 		res = new JList();
-		
+
 		resPane = new JScrollPane(res);
-		
+
 		resListener = new ResultsListener(this, categoryList, dataSet, 2);
 		catListener = new ResultsListener(this, categoryList, dataSet, 1);
 
@@ -76,67 +80,70 @@ public class FirstResults extends JFrame {
 		JButton button = new JButton("Home");
 		button.addActionListener(listener);
 		
+		contentPane.add(resPane, BorderLayout.CENTER);
+		contentPane.add(catPane, BorderLayout.CENTER);
+
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 				gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 								.addGroup(gl_contentPane.createSequentialGroup()
-										.addGap(20)
-										.addComponent(catPane, GroupLayout.PREFERRED_SIZE, 187, GroupLayout.PREFERRED_SIZE)
+										.addGap(8)
+										.addComponent(catPane, GroupLayout.DEFAULT_SIZE, 187, GroupLayout.DEFAULT_SIZE)
 										.addGap(18)
-										.addComponent(resPane, GroupLayout.PREFERRED_SIZE, 191, GroupLayout.PREFERRED_SIZE))
-								.addComponent(button, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE))
-						.addContainerGap(24, Short.MAX_VALUE))
+										.addComponent(resPane, GroupLayout.DEFAULT_SIZE, 191, GroupLayout.DEFAULT_SIZE))
+								.addComponent(button, GroupLayout.DEFAULT_SIZE, 67, GroupLayout.DEFAULT_SIZE))
+						.addContainerGap())
 				);
 		gl_contentPane.setVerticalGroup(
-				gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-						.addComponent(button, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+						.addComponent(button, GroupLayout.DEFAULT_SIZE, 23, GroupLayout.DEFAULT_SIZE)
 						.addPreferredGap(ComponentPlacement.RELATED)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 								.addComponent(resPane, GroupLayout.DEFAULT_SIZE, 242, Short.MAX_VALUE)
-								.addComponent(catPane, GroupLayout.PREFERRED_SIZE, 242, GroupLayout.PREFERRED_SIZE))
+								.addComponent(catPane, GroupLayout.DEFAULT_SIZE, 242, GroupLayout.DEFAULT_SIZE))
 						.addContainerGap())
 				);
 		contentPane.setLayout(gl_contentPane);
 
 	}
-	
+
 	public Results getResults() {
-		
+
 		return results;
-		
+
 	}
-	
+
 	public void setRes(List<String> fileNames) {
-		
+
 		System.out.println("In Set Res");
-		
+
 		System.out.println("FileNames: " + fileNames.toString());
-		
+
 		//res = new JList();
-		
+
 		res.setListData(fileNames.toArray());
-		
+
 		this.repaint();
-		
+
 	}
-	
+
 	public void setFileName(List<String> fileNames) {
-		
+
 		this.fileNames = fileNames;
-		
+
 	}
-	
+
 	public List<String> getFileNames() {
-		
+
 		return fileNames;
-		
+
 	}
-	
+
 	public String getQuery(){
 		return searchTerm;
 	}
-	
+
 }
